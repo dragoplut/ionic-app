537df285012434bb30d7c1a471f1965ccfeb898e
oleksandr.penkivskyy <oleksandr.penkivskyy@vakoms.gmail.com>
2018-01-19 12:21:51 +0200

    Update google api key

--- src/services/google/google.service.ts
+++ src/services/google/google.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Platform } from "ionic-angular";
 import {
   GOOGLE_API_KEY_ANDROID,
-  GOOGLE_API_KEY_IOS,
+  // GOOGLE_API_KEY_IOS,
   GOOGLE_MAP_API_URL
 } from '../../app/constants';
 
@@ -16,8 +16,9 @@ export class GoogleService {
    * Done only once on app start.
    */
   public initGoogleMaps() {
-    const apiKey: string = this.platform.is('ios') ?
-      GOOGLE_API_KEY_IOS : GOOGLE_API_KEY_ANDROID;
+    // const apiKey: string = this.platform.is('ios') ?
+    //   GOOGLE_API_KEY_IOS : GOOGLE_API_KEY_ANDROID;
+    const apiKey: string = GOOGLE_API_KEY_ANDROID;
     let script = document.createElement("script");
     script.id = 'googleMaps';
     script.src = `${GOOGLE_MAP_API_URL}?key=${apiKey}&callback=mapInited`;
